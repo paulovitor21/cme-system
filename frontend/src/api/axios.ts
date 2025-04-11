e79b1788f8683233dd02e54b2d7c716778e33f71
paulovitor21 <paulovitorpereira21@outlook.com>
2025-04-11 11:53:36 -0300

    refactor (frontend): axios

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, // ou use a URL do backend no Docker
+  baseURL: import.meta.env.VITE_API_URL, // URL do backend no Docker
 });
 
 export default api;
